d733e7344a9afc62c0a36a32db088b8b2cbe4832
Basheer Rajeh <muhammadbasheerrajeh@hotmail.com>
2023-06-19 13:52:34 +0300

    Create login and register pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Home from "./page/Home";
+import Login from "./page/Login";
 import Product from "./page/Product";
 import ProductsList from "./page/ProductsList";
+import Register from "./page/Register";
 
 function App() {
   return (
     <>
-      <Product />
+      <Login />
     </>
   );
 }
---
+++ src/page/Register.jsx
@@ -0,0 +1,85 @@
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(175, 25, 25, 0.5),
+      rgba(171, 171, 40, 0.3)
+    ),
+    url("https://images.pexels.com/photos/6984650/pexels-photo-6984650.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940") center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  width: 40%;
+  padding: 20px;
+  background-color: white;
+`;
+
+const Title = styled.h1`
+  text-transform: uppercase;
+  font-size: 24px;
+  font-weight: 300;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 10px 0px 0px;
+  padding: 10px;
+  outline: none;
+`;
+
+const Agreement = styled.span`
+  font-size: 12px;
+  margin: 20px 0px;
+`;
+
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  text-transform: uppercase;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+  transition: all 0.4s ease;
+  
+  &:hover{
+    transform: scale(1.05);
+  }
+`;
+
+const Register = () => {
+    return (
+        <Container>
+            <Wrapper>
+                <Title>Create An Account</Title>
+                <Form>
+                    <Input placeholder="username" />
+                    <Input placeholder="first name" />
+                    <Input placeholder="last name" />
+                    <Input placeholder="password" />
+                    <Input placeholder="confirm password" />
+                    <Agreement>
+                        By creating an account, I consent to the processing of my personal
+                        data in accordance with the <b>PRIVACY POLICY</b>
+                    </Agreement>
+                    <Button>Create</Button>
+                </Form>
+            </Wrapper>
+        </Container>
+    );
+};
+
+export default Register;
